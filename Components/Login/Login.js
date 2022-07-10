073436d6028f033735b593d8ad173fa8b75dc982
Jake Gallagher <jake.a.gallagher98@gmail.com>
2022-07-10 12:10:43 +0100

    Added a guest Login feature to bypass login screen

--- Components/Login/Login.js
+++ Components/Login/Login.js
@@ -13,6 +13,27 @@ const Login = (props) => {
   const axios = require("axios");
   const dispatch = useDispatch();
 
+  /* guest login logic 
+  (this allows anyone to login as a guest with one click, 
+  hopefully this will make it more likely that a recruiter will
+  actually try the website instead of giving up at the login screen) */
+  const guestLoginHandler = async () => {
+    try {
+      /* requesting the data of the Guest user account */
+      const response = await axios.get(
+        "https://twitterclone-ad8de-default-rtdb.europe-west1.firebasedatabase.app/users/Guest.json"
+      );
+      /* setting the data to the redux store */
+      dispatch(setUserData(response.data));
+      /* removing the login screen */
+      props.loginHandler();
+      alert("welcome to the Guest account, \n please feel free to try out any functionalities that you wish")
+    } catch (err) {
+      alert("Something went wrong, Please try again");
+      console.log(err);
+    }
+  };
+
   /* setting the page to Create Account page */
   const signUpPageHandler = () => {
     setSignup(true);
@@ -141,54 +162,59 @@ const Login = (props) => {
   };
 
   return (
-    <div className={classes.page}>
-      <div className={classes.topButtons}>
-        <button onClick={signUpPageHandler}>Create Account</button>
-        <button onClick={loginPageHandler}>Login</button>
-      </div>
-      {/* this is the login screen */}
-      {!signup && (
-        <form className={classes.loginForm}>
-          <label htmlFor="username">Username: </label>
-          <input type="text" onChange={usernameHandler} id="username"></input>
-          <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            onChange={passwordHandler}
-            value={password}
-            id="password"
-          ></input>
-          <button onClick={signInHandler}>Sign in</button>
-        </form>
-      )}
+    <div className={classes.container}>
+      <button className={classes.guestLogin} onClick={guestLoginHandler}>
+        Guest Login
+      </button>
+      <div className={classes.page}>
+        <div className={classes.topButtons}>
+          <button onClick={signUpPageHandler}>Create Account</button>
+          <button onClick={loginPageHandler}>Login</button>
+        </div>
+        {/* this is the login screen */}
+        {!signup && (
+          <form className={classes.loginForm}>
+            <label htmlFor="username">Username: </label>
+            <input type="text" onChange={usernameHandler} id="username"></input>
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              onChange={passwordHandler}
+              value={password}
+              id="password"
+            ></input>
+            <button onClick={signInHandler}>Sign in</button>
+          </form>
+        )}
 
-      {/* this is the create account screen */}
-      {signup && (
-        <form className={classes.createAccountForm}>
-          <label htmlFor="createUsername">Create Username: </label>
-          <input
-            type="text"
-            onChange={createdUsernameHandler}
-            id="createUsername"
-          ></input>
-          <label htmlFor="createPassword">Create Password: </label>
-          <input
-            type="password"
-            placeholder="Min 8 Characters"
-            onChange={createdPasswordHandler}
-            value={createdPassword}
-            id="createPassword"
-          ></input>
-          <label htmlFor="retypePassword">Retype Password: </label>
-          <input
-            type="password"
-            onChange={retypedPasswordHandler}
-            value={retypedPassword}
-            id="retypePassword"
-          ></input>
-          <button onClick={registerHandler}>register</button>
-        </form>
-      )}
+        {/* this is the create account screen */}
+        {signup && (
+          <form className={classes.createAccountForm}>
+            <label htmlFor="createUsername">Create Username: </label>
+            <input
+              type="text"
+              onChange={createdUsernameHandler}
+              id="createUsername"
+            ></input>
+            <label htmlFor="createPassword">Create Password: </label>
+            <input
+              type="password"
+              placeholder="Min 8 Characters"
+              onChange={createdPasswordHandler}
+              value={createdPassword}
+              id="createPassword"
+            ></input>
+            <label htmlFor="retypePassword">Retype Password: </label>
+            <input
+              type="password"
+              onChange={retypedPasswordHandler}
+              value={retypedPassword}
+              id="retypePassword"
+            ></input>
+            <button onClick={registerHandler}>register</button>
+          </form>
+        )}
+      </div>
     </div>
   );
 };
